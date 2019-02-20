28c98768783508cee7b4b118ef1c529c9c0c9ae0
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2019-02-20 14:47:47 +0100

    feat(helper): add helper and cui-modal component
    * add helper services
    * add cui-modal
    * add cucConfig provider
    
    * add `jsurl@^0.1.5` peer dependency
    * add `@ovh-ux/ng-translate-async-loader@^2.0.0` peer dependency
    * add `angular-translate@^2.11.0` peer dependency
    * add `angular-ui-bootstrap@~1.3.3` peer dependency
    
    ref: OM-306

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import angular from 'angular';
 
-import cui from './cui';
+import cucConfig from './config';
+import cucHelper from './helper';
 import cucMessage from './message';
 import cucNavigation from './navigation';
 import cucPoll from './poll';
 import cucProducts from './products';
+import cui from './cui';
 
 const moduleName = 'ngOvhCloudUniverseComponents';
 
 angular
   .module(moduleName, [
-    cui,
+    cucConfig,
+    cucHelper,
     cucMessage,
     cucNavigation,
     cucPoll,
     cucProducts,
+    cui,
   ]);
 
 export default moduleName;
